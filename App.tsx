d6decb1630e9fa4f49649414a5542e7da9ec5601
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-05-13 23:03:53 -0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,16 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, KeyboardAvoidingView, TextInput, StyleSheet, Text, Platform, TouchableWithoutFeedback, Button, Keyboard  } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View style={styles.container}> n
+  <KeyboardAvoidingView
+     behavior={Platform.OS === "ios" ? "padding" : "height"}
+     style={styles.container}
+   >
+     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+       <View style={styles.inner}>
+         <Text style={styles.header}>Header</Text>
+         <TextInput placeholder="Username" style={styles.textInput} />
+         <View style={styles.btnContainer}>
+           <Button title="Submit" onPress={() => null} />
+         </View>
+       </View>
+     </TouchableWithoutFeedback>
+   </KeyboardAvoidingView>
 
+    </View>
+ 
+  );
+};
+
+    
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +34,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: "space-around"
+  },
+  header: {
+    fontSize: 36,
+    marginBottom: 48
+  },
+  textInput: {
+    height: 40,
+    borderColor: "#000000",
+    borderBottomWidth: 1,
+    marginBottom: 36
+  },
+  btnContainer: {
+    backgroundColor: "white",
+    marginTop: 12
+  }
+  
 });
