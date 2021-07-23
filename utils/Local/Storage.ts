046f16db27b43117538da7ae2673559c55e69d07
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-07-23 17:52:48 -0400

    fix: deleting note from storage

--- utils/Local/Storage.ts
+++ utils/Local/Storage.ts
@@ -1,36 +1,32 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { note } from "../notesContext";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { note } from '../notesContext';
 
-export const storeNotes = async (value : Array<note>) =>{
-    try {
-        const valueJSON = JSON.stringify(value);
-        await AsyncStorage.setItem('@Notes', valueJSON);
-    } catch (error) {
-        console.log(error);
-    }
-} 
-
+export const storeNotes = async (value: Array<note>) => {
+	try {
+		const valueJSON = JSON.stringify(value);
+		await AsyncStorage.setItem('@Notes', valueJSON);
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 export const getNotes = async () => {
+	try {
+		const jsonValue = await AsyncStorage.getItem('@Notes');
+		return jsonValue != null ? JSON.parse(jsonValue) : null;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-    try {
-        const jsonValue = await AsyncStorage.getItem('@Notes');
-        return jsonValue != null ? JSON.parse(jsonValue) : null; 
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-export const deleteNote = async (id : number) => {
-    try {
-        const request = await getNotes();
-        const newNotes = request.filter((item: note) =>{
-            item.id !== id;
-        }) ;
-        storeNotes(newNotes)
-    } catch (error) {
-        console.log(error)
-    }
-}  
-
-
+export const deleteNote = async (id: string) => {
+	try {
+		const request = await getNotes();
+		const newNotes = request.filter((item: note) => {
+			return item.id !== id;
+		});
+		storeNotes(newNotes);
+	} catch (error) {
+		console.log(error);
+	}
+};
