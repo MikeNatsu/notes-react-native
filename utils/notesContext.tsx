fd8cc6cc4a73f3c5cab063b854d8792db735f8eb
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-07-23 17:53:21 -0400

    add: isDeletable boolean

--- utils/notesContext.tsx
+++ utils/notesContext.tsx
@@ -1,32 +1,37 @@
-import React,{ useState, createContext, ReactNode } from 'react'
+import React, { useState, createContext, ReactNode } from 'react';
 
 type Props = {
-    children: ReactNode;
+	children: ReactNode;
+};
 
+export interface note {
+	id: string;
+	title: string;
+	text: string;
 }
 
-export interface note{
-    id: number | string;
-    title: string;
-    text: string;  
-}
-
-interface NoteContextFunction{
-    notes: note[];
-    setNotes: (notes : note[]) => void; 
+interface NoteContextFunction {
+	notes: note[];
+	setNotes: (notes: note[]) => void;
+	isDeletable: boolean;
+	enableDelete: (turn: boolean) => void;
 }
 
 export const notesContext = createContext<NoteContextFunction>({
-    notes:[],
-    setNotes: ()=>{},
+	notes: [],
+	setNotes: () => {},
+	isDeletable: false,
+	enableDelete: () => {},
 });
 
-
-export default function NotesProvider ({children} : Props){ 
-    const [notes, setNotes ] = useState<note[]>([]);
-    return (
-        <notesContext.Provider value={{notes, setNotes}}>
-            {children}
-        </notesContext.Provider>
-    ); 
+export default function NotesProvider({ children }: Props) {
+	const [notes, setNotes] = useState<note[]>([]);
+	const [isDeletable, enableDelete] = useState<boolean>(false);
+	return (
+		<notesContext.Provider
+			value={{ notes, setNotes, isDeletable, enableDelete }}
+		>
+			{children}
+		</notesContext.Provider>
+	);
 }
